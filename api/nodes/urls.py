4f4a8de308b9ef9980e185410c2ca661fa1b360f
unknowncoder05 <yersonlasso05@gmail.com>
2022-07-31 20:47:40 -0500

    added node relationship viewsets

--- api/nodes/urls.py
+++ api/nodes/urls.py
@@ -5,6 +5,7 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 
 router.register(r'nodes', views.NodesViewSet, basename='nodes')
+router.register(r'relationships', views.NodeRelationshipsViewSet, basename='nodes_relationships')
 
 
 urlpatterns = [
--- api/nodes/views/node.py
+++ api/nodes/views/node.py
@@ -3,10 +3,12 @@ import re
 from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.generics import get_object_or_404
+from django.db.models import Q
 
-from api.nodes.models import Node
+from api.nodes.models import Node, ProposedRelationship
 from api.nodes.serializers import (
-    ListNodeSerializer, DescribeNodeSerializer, WriteNodeSerializer
+    ListNodeSerializer, DescribeProposedRelationshipSerializer, WriteProposedRelationshipSerializer, DescribeNodeSerializer, WriteNodeSerializer
 )
 
 
@@ -20,3 +22,22 @@ class NodesViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return DescribeNodeSerializer
         return super().get_serializer_class()
+
+
+class NodeRelationshipsViewSet(viewsets.ModelViewSet):
+    queryset = Node.objects.all()
+    serializer_class = DescribeProposedRelationshipSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'partial_update', 'update']:
+            return WriteProposedRelationshipSerializer
+        if self.action == 'retrieve':
+            return DescribeProposedRelationshipSerializer
+        return super().get_serializer_class()
+
+    def get_queryset(self, **kwargs):
+        print("kwargs", kwargs)
+        if 'node' in kwargs:
+            self.node = get_object_or_404(Node, id=kwargs['node'])
+            return ProposedRelationship.objects.filter(Q(from_node=self.node) | Q(to_node=self.node))
+        return ProposedRelationship.objects.all()
